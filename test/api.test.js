399631fadf41b63c9a7f74819c61498af4b290e5
Edrees Jalili <edreesjalili@gmail.com>
2019-04-13 16:57:57 -0500

    update tests

--- test/api.test.js
+++ test/api.test.js
@@ -1,6 +1,8 @@
 import { expect } from "chai";
 import { init } from "../lib/server";
 
+const API_ROUTE = "/api/v1/pharmacies";
+
 describe("API /pharmacies", () => {
   let server;
 
@@ -13,13 +15,32 @@ describe("API /pharmacies", () => {
   });
 
   describe("GET /pharmacies", () => {
-    it("responds with status code 200", async () => {
-      const res = await server.inject({
-        method: "GET",
-        url: "/api/v1/pharmacies"
-      });
+    let subject;
 
-      expect(res.statusCode).to.equal(200);
+    beforeEach("Send GET", async () => {
+      subject = await server.inject({
+        method: "GET",
+        url: API_ROUTE
+      });
+    });
+
+    it("responds with status code 200", async () =>
+      expect(subject.statusCode).to.equal(200));
+
+    it("responds with all available pharmacies", async () => {
+      allPharmacies = await getAllPharmacies();
+      expect(subject.payload).to.equal(allPharmacies);
     });
   });
 });
+
+const getAllPharmacies = () =>
+  new Promise((resolve, reject) => {
+    const entries = [];
+
+    fs.createReadStream(path.join(__dirname, "data", "pharmacies.csv"))
+      .pipe(csv())
+      .on("data", data => entries.push(data))
+      .on("error", reject)
+      .on("end", () => resolve(entries));
+  });
